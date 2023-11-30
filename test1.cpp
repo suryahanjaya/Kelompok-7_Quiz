df5fa9418b5eda9ac49dc383cd6dec4326c3b4df
Surya Hanjaya <151160135+suryahanjaya@users.noreply.github.com>
2023-11-30 13:40:39 +0700

    >github

--- test1.cpp
+++ test1.cpp
@@ -1,2 +1,18 @@
-t
+// 01 Tukar Nilai
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    int *p1 = &x, *p2 = &y;
+    cin     >> x >> y;
+
+    int a   = *p1;
+        *p1 = *p2;
+        *p2 = a;
+    cout    << "x = " << x << endl 
+            << "y = " << y << endl;
+
+}
+
 
